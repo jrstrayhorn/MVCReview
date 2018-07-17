36a641a42019edacbcbf3382043e0bc1e898241e
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-17 07:10:39 -0400

    wip

---
+++ MVCReview/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using MVCReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReview.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            // real world model would come from database
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
