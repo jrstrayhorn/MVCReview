c6d289e8c89f9281423c4127917f13f510d9ff12
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-18 07:36:32 -0400

    wip - finished with asp.net fundamentals

--- MVCReview/App_Start/RouteConfig.cs
+++ MVCReview/App_Start/RouteConfig.cs
@@ -13,6 +13,26 @@ namespace MVCReview
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // use attribute routing moving forward
+            routes.MapMvcAttributeRoutes();
+
+            // creating custom route (order matters) w/ constraint
+            // convention based
+            // constraints use regular expressions
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" });
+
+
+            // this code is fragile because of the magic strings
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2015|2016", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- MVCReview/Controllers/MoviesController.cs
+++ MVCReview/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using MVCReview.Models;
+using MVCReview.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,42 @@ namespace MVCReview.Controllers
         {
             // real world model would come from database
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //// another way to pass data is with view dictionary
+            //// every controller has ViewData property
+            //ViewData["Movie"] = movie;
+
+            //// this property is added at run time because ViewBag is dynamic
+            //// so we don't get compile time safety/checking
+            //ViewBag.Movie = movie;
+
+            // DONT USE VIEWDATA OR VIEWBAG results in fragile code
+
+            //return View();
+
+            //// Under the hood, MVC stores movie as a property of ViewData.Model
+            //// ViewData is a ViewDataDictionary and can also be used as key, value
+            //// pair.... using return View(movie) - automatically places object as ViewData.Model
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model
+
+            // this is the one better way to pass data to a view
+            //return View(movie);
+
+            // going to send the view model now
+            return View(viewModel);
+
             //return Content("Hello World!");
             //return HttpNotFound();
             // empty result
@@ -38,6 +73,13 @@ namespace MVCReview.Controllers
                 sortBy = "Name";
 
             return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        // attribute routing
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
         }
     }
 }
