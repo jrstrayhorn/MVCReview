85865dcb322a4438b2cf2b979dfa7fe2ebe6c6f0
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-20 08:28:10 -0400

    done

---
+++ MVCReview/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using MVCReview.Models;
+using MVCReview.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReview.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/{Id}
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+                return HttpNotFound();
+
+            var customer = GetCustomers()
+                            .SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
--- MVCReview/Controllers/MoviesController.cs
+++ MVCReview/Controllers/MoviesController.cs
@@ -64,15 +64,32 @@ namespace MVCReview.Controllers
         }
 
         // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        //}
+
+        // movies
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            return View(movies);
+        }
 
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
 
         // attribute routing
