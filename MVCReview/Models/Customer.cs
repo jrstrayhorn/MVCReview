0ae81df5a0858afeca54b27dfda3aa0b7b5b53eb
stepforwardtech <jrstrayhorn@gmail.com>
2018-08-15 07:44:45 -0400

    done with implementing validation, custom validation

--- MVCReview/Models/Customer.cs
+++ MVCReview/Models/Customer.cs
@@ -27,6 +27,7 @@ namespace MVCReview.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
