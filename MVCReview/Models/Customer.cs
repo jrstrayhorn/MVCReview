5b1fae8bd4469f9728fb30e4121e1489666e2f89
stepforwardtech <jrstrayhorn@gmail.com>
2018-08-15 07:36:08 -0400

    done with implementing validation, data annotations

--- MVCReview/Models/Customer.cs
+++ MVCReview/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace MVCReview.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
