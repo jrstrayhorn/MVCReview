1949be519be3ea8e7ebcbc7181eb6f4b1529a162
stepforwardtech <jrstrayhorn@gmail.com>
2018-08-16 07:15:29 -0400

    done with implementing validation, validation summary

--- MVCReview/Controllers/CustomersController.cs
+++ MVCReview/Controllers/CustomersController.cs
@@ -28,6 +28,7 @@ namespace MVCReview.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm", viewModel);
