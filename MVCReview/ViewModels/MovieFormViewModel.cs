4be767688a240eaa4f82925c7af0c3ff6325c3b9
stepforwardtech <jrstrayhorn@gmail.com>
2018-08-17 07:31:53 -0400

    done with validation exercise

--- MVCReview/ViewModels/MovieFormViewModel.cs
+++ MVCReview/ViewModels/MovieFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MVCReview.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,45 @@ namespace MVCReview.ViewModels
 {
     public class MovieFormViewModel
     {
-        public Movie Movie { get; set; }
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        [Required]
+        public int? NumberInStock { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public int? GenreId { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit Movie" : "New Movie";
+            }
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
     }
 }
