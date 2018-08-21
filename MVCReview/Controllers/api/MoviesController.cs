2bb752344ea91a3f92a0a1b4c7a062aa4e363ecf
stepforwardtech <jrstrayhorn@gmail.com>
2018-08-21 07:49:43 -0400

    wip restful service exercise

---
+++ MVCReview/Controllers/api/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MVCReview.Dtos;
+using MVCReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCReview.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/movies
+        public IHttpActionResult GetMovies()
+        {
+            return Ok(_context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>));
+        }
+
+        // GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        // POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri($"{Request.RequestUri}/{movie.Id}"), movieDto);
+        }
+    }
+}
---
+++ MVCReview/Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCReview.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        //public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public int GenreId { get; set; }
+    }
+}
